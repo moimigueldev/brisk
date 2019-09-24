3f0887e6a384fdefff8efc7070cb254d0c58bfeb
Moises Miguel <moimigueldev@gmail.com>
2019-09-24 10:28:26 -0500

    desktop layout is for login/signup is complete

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FirebaseAuthService } from 'src/app/services/firebase-auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +16,20 @@ export class LoginComponent implements OnInit {
     password: new FormControl('', [Validators.required, Validators.minLength(8)])
   })
 
-  constructor(private fas: FirebaseAuthService) { }
+  constructor(
+    private fas: FirebaseAuthService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
     this.fas.signIn(this.loginForm.value);
+  }
+
+  goToSignup() {
+    this.router.navigate(['signup']);
   }
 
 }
